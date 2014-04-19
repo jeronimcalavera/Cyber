6ad3bcf97cfffe8dff8832d34c191122e0a5e8b3
jeronimcalavera <sonofsudeki1@rambler.ru>
2014-04-19 17:36:36 +0400

    Unity files

---
+++ UNITY/New Unity Project 5/Assets/Script/MyDialogueSystem.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MyDialogueSystem : MonoBehaviour 
+{
+	public bool debugMode = false;
+	public Texture2D Image = null;
+	public Rect position = new Rect (10, 10, 150, 150);
+	public bool ShowGUI = false; 
+	public bool checkKeyDown = false;
+
+	void OnTriggerEnter (Collider other) //enter the trigger
+	{			
+				if ((other.gameObject.name == "Collider_player"))
+				{
+					ShowGUI = true;
+					 Debug.Log ("no way");
+		  }
+		}
+	void OnTriggerExit (Collider other) //exit the trigger
+	{
+		if ((other.gameObject.name == "Collider_player"))
+		{
+			ShowGUI = false;
+			 Debug.Log ("ah, go away");
+		}
+	}
+
+	void Update (){ //press enter to execute
+		if (Input.GetKey("return") && ShowGUI == true)
+		    {
+			if (!checkKeyDown){
+		    checkKeyDown = true;
+	   		 Debug.Log ("BUTTSEX");
+			}
+		  }
+		if (ShowGUI == false) {
+			checkKeyDown = false;		
+		}
+	   }
+
+
+	void OnGUI()//GUI box
+		{
+		if (debugMode || Application.isPlaying){
+		if (ShowGUI == true && checkKeyDown==true) 
+		 {
+				GUI.Box (position, Image);
+
+			 }
+			}
+		}
+	}
